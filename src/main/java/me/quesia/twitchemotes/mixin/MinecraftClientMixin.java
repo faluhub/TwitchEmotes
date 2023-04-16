3c2058ad74cea5b168129882a45a06562dfce0d1
wurgo <laxvideoeffects@gmail.com>
2023-04-16 14:35:24 +0100

    fix silly compat issue

--- src/main/java/me/quesia/twitchemotes/mixin/MinecraftClientMixin.java
+++ src/main/java/me/quesia/twitchemotes/mixin/MinecraftClientMixin.java
@@ -3,11 +3,13 @@ package me.quesia.twitchemotes.mixin;
 import me.quesia.twitchemotes.TwitchEmotes;
 import me.quesia.twitchemotes.owner.TwitchMessageListOwner;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.RunArgs;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.gui.screen.FatalErrorScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.text.LiteralText;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -17,8 +19,10 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
-public class MinecraftClientMixin {
+public abstract class MinecraftClientMixin {
     @Shadow @Final public InGameHud inGameHud;
+
+    @Shadow public abstract void openScreen(@Nullable Screen screen);
 
     @Inject(method = "joinWorld", at = @At("HEAD"))
     private void clearChat(ClientWorld world, CallbackInfo ci) {
@@ -27,11 +31,13 @@ public class MinecraftClientMixin {
         }
     }
 
-    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;openScreen(Lnet/minecraft/client/gui/screen/Screen;)V", ordinal = 1))
-    private void openFatalScreen(MinecraftClient instance, Screen screen) {
-        instance.openScreen(TwitchEmotes.TWIRK.isConnected() ? screen : new FatalErrorScreen(
-                new LiteralText("Couldn't connect to Twitch chat!"),
-                new LiteralText("This is a problem with Twitch itself and can only be fixed by relaunching the game.")
-        ));
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void openFatalScreen(RunArgs args, CallbackInfo ci) {
+        if (!TwitchEmotes.TWIRK.isConnected()) {
+            this.openScreen(new FatalErrorScreen(
+                    new LiteralText("Couldn't connect to Twitch chat!"),
+                    new LiteralText("This is a problem with Twitch itself and can only be fixed by relaunching the game.")
+            ));
+        }
     }
 }

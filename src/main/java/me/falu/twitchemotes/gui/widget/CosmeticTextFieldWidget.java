8be0fead26bfcc3d7fc7238590075e40f611ddb5
faluhub <laxvideoeffects@gmail.com>
2024-03-06 19:56:29 +0100

    remove unnecessary stuff

--- src/main/java/me/falu/twitchemotes/gui/widget/CosmeticTextFieldWidget.java
+++ src/main/java/me/falu/twitchemotes/gui/widget/CosmeticTextFieldWidget.java
@@ -21,11 +21,6 @@ public class CosmeticTextFieldWidget extends TextFieldWidget {
         super(textRenderer, x + 1, y + 1, width - 2, height - 2, Text.literal(""));
     }
 
-    @SuppressWarnings("unused")
-    public CosmeticTextFieldWidget(TextRenderer textRenderer, int x, int y, int width, int height, @Nullable TextFieldWidget copyFrom) {
-        super(textRenderer, x + 2, y + 2, width - 4, height - 4, copyFrom, Text.literal(""));
-    }
-
     public void addTextAsLines(String text) {
         String[] parts = text.split("\n");
         for (String part : parts) {

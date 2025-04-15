2bfd0878e23765cf86adb144c9b4c8e8f028cbb1
falu <71964154+faluhub@users.noreply.github.com>
2025-04-15 15:28:07 +0200

    oops

--- src/main/java/me/falu/twitchemotes/mixin/chat/ChatScreenMixin.java
+++ src/main/java/me/falu/twitchemotes/mixin/chat/ChatScreenMixin.java
@@ -6,10 +6,12 @@ import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,6 +19,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ChatScreen.class)
 public abstract class ChatScreenMixin extends Screen {
+    @Shadow protected TextFieldWidget chatField;
     @Unique private static final Identifier BUTTON_ICON = new Identifier("textures/item/feather.png");
     @Unique private ButtonWidget button;
 
@@ -31,7 +34,7 @@ public abstract class ChatScreenMixin extends Screen {
                 this.client.setScreen(new MenuSelectionScreen());
             }
         }).dimensions(0, 0, 20, 20).build());
-        this.setInitialFocus();
+        this.setInitialFocus(this.chatField);
     }
 
     @Override

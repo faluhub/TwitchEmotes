109c95322832a5c8c2de787a472429d7cc6b3e10
falu <71964154+faluhub@users.noreply.github.com>
2025-04-15 15:23:52 +0200

    fix #3 and 7tv crash

--- src/main/java/me/falu/twitchemotes/mixin/chat/ChatScreenMixin.java
+++ src/main/java/me/falu/twitchemotes/mixin/chat/ChatScreenMixin.java
@@ -2,11 +2,13 @@ package me.falu.twitchemotes.mixin.chat;
 
 import me.falu.twitchemotes.gui.screen.MenuSelectionScreen;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,6 +18,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ChatScreen.class)
 public abstract class ChatScreenMixin extends Screen {
     @Unique private static final Identifier BUTTON_ICON = Identifier.of("textures/item/feather.png");
+    @Unique private ButtonWidget button;
 
     protected ChatScreenMixin(Text title) {
         super(title);
@@ -23,11 +26,19 @@ public abstract class ChatScreenMixin extends Screen {
 
     @Inject(method = "init", at = @At("TAIL"))
     public void addConfigButton(CallbackInfo ci) {
-        this.addDrawableChild(ButtonWidget.builder(Text.literal(""), b -> {
+        this.addDrawableChild(this.button = ButtonWidget.builder(Text.literal(""), b -> {
             if (this.client != null) {
                 this.client.setScreen(new MenuSelectionScreen());
             }
         }).dimensions(0, 0, 20, 20).build());
+        this.setInitialFocus();
+    }
+
+    @Override
+    public void setFocused(@Nullable Element focused) {
+        if (focused != this.button) {
+            super.setFocused(focused);
+        }
     }
 
     @Inject(method = "render", at = @At("TAIL"))

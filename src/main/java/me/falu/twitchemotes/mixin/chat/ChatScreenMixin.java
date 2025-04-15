1cd4bd547e43dad1a0d7a41b35f6180cb7f66e64
falu <71964154+faluhub@users.noreply.github.com>
2025-04-15 17:19:16 +0200

    fix #3 and 7tv crash

--- src/main/java/me/falu/twitchemotes/mixin/chat/ChatScreenMixin.java
+++ src/main/java/me/falu/twitchemotes/mixin/chat/ChatScreenMixin.java
@@ -3,14 +3,18 @@ package me.falu.twitchemotes.mixin.chat;
 import me.falu.twitchemotes.TwitchEmotes;
 import me.falu.twitchemotes.TwitchEmotesOptions;
 import me.falu.twitchemotes.gui.screen.MenuSelectionScreen;
+import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,7 +22,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ChatScreen.class)
 public abstract class ChatScreenMixin extends Screen {
+    @Shadow protected TextFieldWidget chatField;
     @Unique private static final Identifier BUTTON_ICON = new Identifier("textures/item/feather.png");
+    @Unique private ButtonWidget button;
 
     protected ChatScreenMixin(Text title) {
         super(title);
@@ -34,11 +40,19 @@ public abstract class ChatScreenMixin extends Screen {
 
     @Inject(method = "init", at = @At("TAIL"))
     public void addConfigButton(CallbackInfo ci) {
-        this.addButton(new ButtonWidget(0, 0, 20, 20, new LiteralText(""), b -> {
+        this.addButton(this.button = new ButtonWidget(0, 0, 20, 20, new LiteralText(""), b -> {
             if (this.client != null) {
                 this.client.openScreen(new MenuSelectionScreen());
             }
         }));
+        this.setInitialFocus(this.chatField);
+    }
+
+    @Override
+    public void setFocused(@Nullable Element focused) {
+        if (focused != this.button) {
+            super.setFocused(focused);
+        }
     }
 
     @Inject(method = "render", at = @At("TAIL"))

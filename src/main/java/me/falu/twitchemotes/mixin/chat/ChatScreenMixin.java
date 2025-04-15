ebbbe18294de11c1f18cacfb5e2de2e80b89a125
falu <71964154+faluhub@users.noreply.github.com>
2025-04-15 16:33:27 +0200

    fix #3 and 7tv crash

--- src/main/java/me/falu/twitchemotes/mixin/chat/ChatScreenMixin.java
+++ src/main/java/me/falu/twitchemotes/mixin/chat/ChatScreenMixin.java
@@ -2,13 +2,17 @@ package me.falu.twitchemotes.mixin.chat;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import me.falu.twitchemotes.gui.screen.MenuSelectionScreen;
+import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,6 +21,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ChatScreen.class)
 public abstract class ChatScreenMixin extends Screen {
     @Unique private static final Identifier BUTTON_ICON = new Identifier("textures/item/feather.png");
+    @Shadow protected TextFieldWidget chatField;
+    @Unique private ButtonWidget button;
 
     protected ChatScreenMixin(Text title) {
         super(title);
@@ -24,11 +30,19 @@ public abstract class ChatScreenMixin extends Screen {
 
     @Inject(method = "init", at = @At("TAIL"))
     public void addConfigButton(CallbackInfo ci) {
-        this.addDrawableChild(new ButtonWidget(0, 0, 20, 20, Text.literal(""), b -> {
+        this.addDrawableChild(this.button = new ButtonWidget(0, 0, 20, 20, Text.literal(""), b -> {
             if (this.client != null) {
                 this.client.setScreen(new MenuSelectionScreen());
             }
         }));
+        this.setInitialFocus(this.chatField);
+    }
+
+    @Override
+    public void setFocused(@Nullable Element focused) {
+        if (focused != this.button) {
+            super.setFocused(focused);
+        }
     }
 
     @Inject(method = "render", at = @At("TAIL"))
